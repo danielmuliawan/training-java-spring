d432beabc02dff4ddae4bc4fbe1cdf63ad43b8a2
Daniel Muliawan <daniel.muliawan@mitrais.com>
2017-09-12 13:34:09 +0800

    Controller and Web

---
+++ src/main/java/com/example/springmvctraining/HelloController.java
@@ -0,0 +1,26 @@
+package com.example.springmvctraining;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+	
+	@RequestMapping(value="/hello", method=RequestMethod.GET)
+    @ResponseBody
+    public String hello(@RequestParam String message) {
+        return "Hello " + message + "!";
+    }
+	
+//    @RequestMapping(value="/hello/{message}", method=RequestMethod.GET)
+//    @ResponseBody
+//    public String hello(@PathVariable String message) {
+//        return "Hello " + message + "!";
+//    }
+   
+}
---
+++ src/test/java/com/example/springmvctraining/SpringmvctrainingApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.springmvctraining;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringmvctrainingApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
